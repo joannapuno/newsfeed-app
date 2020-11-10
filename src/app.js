0c0c86d2aa7cdeb11a053cad0cb19a97595d844c
Joanna <j.puno0909@gmail.com>
2020-11-10 16:48:46 -0700

    add functionality to add post

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
-const addPostHandler = () => {
-    const newPostBox = document.getElementById('new-post-container');
+const newPostBox = document.getElementById('new-post-container');
+const bottomNav = document.getElementById('bottom-nav');
+const addPostBtns = document.getElementsByClassName('add-post-btn');
+const dismissAddNewBtn = document.getElementById('dismiss-new-post-btn');
+
+const showAddNewPostHandler = () => {
     if(!newPostBox.classList.contains('show')) {
+        bottomNav.classList.add('hide');
         newPostBox.classList.add('show');
     }
 }
 
-const addPostBtns = document.getElementsByClassName('add-post-btn');
+const dismissAddNewHandler = () => {
+    bottomNav.classList.remove('hide');
+    newPostBox.classList.remove('show');
+}
 
 for(let addBtn of addPostBtns) {
-    addBtn.addEventListener('click', evt => {
-        addPostHandler();
-    })
+    addBtn.addEventListener('click', evt => showAddNewPostHandler() )
 }
+
+dismissAddNewBtn.addEventListener('click', evt => dismissAddNewHandler() );
