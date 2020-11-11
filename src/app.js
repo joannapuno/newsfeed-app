34d4dd403bf015df12e6b12b524bed04b7318186
Joanna <j.puno0909@gmail.com>
2020-11-11 13:52:30 -0700

    add icons

--- src/app.js
+++ src/app.js
@@ -7,29 +7,54 @@ const noPostsView = document.getElementById('no-posts-container');
 const newPostForm = document.getElementById('new-post-form');
 const publishPostBtn = document.getElementById('publish-post-btn');
 const newPostCaption = document.getElementById('txt-new-post');
-const newPostAttachment = document.getElementById('post-img');
+const newPostAttachment = document.getElementById('post-attachment');
 
 ///// Show Add New Post Field /////
 const showAddNewFormHandler = () => {
-
-    if(!newPostBox.classList.contains('show')) {
+    
+    if (!newPostBox.classList.contains('show')) {
         bottomNav.classList.add('hide');
         newPostBox.classList.add('show');
     }
     newPostForm.reset();
     publishPostBtn.disabled = true;
-
-    [newPostCaption, newPostAttachment].forEach(element => {
-        element.addEventListener('change', evt => {
+    
+    document.querySelector('.attached-files-container') ? 
+    document.querySelector('.attached-files-container').remove() : null;
+    
+    newPostCaption.addEventListener('input', () => {
+        publishPostBtn.disabled = false;
+    })
+    newPostAttachment.addEventListener('change', () => {
+        let previewContainer = document.querySelector('.new-post-caption');
             publishPostBtn.disabled = false;
+            let selectedImgs = createImgSrcHandler([...newPostAttachment.files]);
+            previewContainer.appendChild(selectedImgs);
         })
-    });
 }
 
 const dismissAddNewHandler = () => {
     bottomNav.classList.remove('hide');
     newPostBox.classList.remove('show');
 }
+
+const createImgSrcHandler = files => {
+    let attachmentContainer = document.createElement('div');
+    attachmentContainer.classList.add('attached-files-container');
+
+    for (let file of files) {
+        let img = document.createElement('img');
+        let imgContainer = document.createElement('div');
+
+        imgContainer.classList.add('img-container');
+        img.src = URL.createObjectURL(file)
+
+        imgContainer.appendChild(img)
+        attachmentContainer.appendChild(imgContainer)
+    };
+
+    return attachmentContainer;
+};
 
 ///// Add New Post /////
 const createNewPostHandler = formData => {
@@ -44,50 +69,48 @@ const createNewPostHandler = formData => {
     const caption = card_c.querySelector('.caption');
     const attachment = card_c.querySelector('.card-attachment');
 
-    
+
     dateStamp.innerText = formData.date;
     caption.style.display = 'none';
     attachment.style.display = 'none';
-    
+
     //Check which has content
-    if(formData.attachment){
+    if (formData.attachment.length) {
         attachment.style.display = 'block';
-        var myImage = document.createElement('img');
-        myImage.src = URL.createObjectURL(formData.attachment)
-        attachment.appendChild(myImage);
+        let imgSrcs = createImgSrcHandler(formData.attachment);
+        attachment.appendChild(imgSrcs);
     }
 
-    if(formData.caption) {
+    if (formData.caption) {
         caption.style.display = 'block';
         caption.innerText = formData.caption;
     }
 
-    cardsWrapper.appendChild(card_c);
+    cardsWrapper.prepend(card_c);
 
 };
 
 
-
 addPostBtns.forEach(btn => {
-    btn.addEventListener('click',showAddNewFormHandler, false)
+    btn.addEventListener('click', showAddNewFormHandler, false)
 })
-dismissAddNewBtn.addEventListener('click',dismissAddNewHandler, false);
+dismissAddNewBtn.addEventListener('click', dismissAddNewHandler, false);
 
 publishPostBtn.addEventListener('click', evt => {
+    evt.preventDefault();
+
     const todayDate = new Date();
-    const dateFormatted = todayDate.toLocaleString('default', 
-    { 
-        month: 'long', day: 'numeric', year: 'numeric' 
-    } ); 
+    const dateFormatted = todayDate.toLocaleString('default',
+        {
+            month: 'long', day: 'numeric', year: 'numeric'
+        });
     let formData = new FormData();
 
     formData.caption = newPostCaption.value;
-    formData.attachment = newPostAttachment.files[0];
+    formData.attachment = [...newPostAttachment.files];
     formData.date = dateFormatted;
-    console.log(formData)
 
     noPostsView.style.display = 'none';
-    
 
     createNewPostHandler(formData);
     dismissAddNewHandler();
